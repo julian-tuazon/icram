81201413962bb7448979a9f5eb48b133075f7e39
Julian <jthrow00@gmail.com>
2020-03-04 14:03:19 -0800

    Added bootstrap styling classes to span tags in Nav

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -12,10 +12,10 @@ export default class Nav extends React.Component {
 
   render() {
     return (
-      <div>
-        <span id='view-cards' onClick={this.handleClick}>View Cards</span>
-        <span id='review-cards' onClick={this.handleClick}>Review</span>
-        <span id='create-card' onClick={this.handleClick}>Create Card</span>
+      <div className='d-flex justify-content-center my-4'>
+        <span className='mr-2 btn btn-primary' id='view-cards' onClick={this.handleClick}>View Cards</span>
+        <span className='mr-2 btn btn-info' id='review-cards' onClick={this.handleClick}>Review</span>
+        <span className='btn btn-success' id='create-card' onClick={this.handleClick}>Create Card</span>
       </div>
     );
   }
