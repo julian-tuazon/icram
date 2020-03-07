a14db0d89600e1480cb6e94662fe2c9f510bd976
Julian <jthrow00@gmail.com>
2020-03-07 14:18:33 -0800

    Created jsx skeleton for rendering cards in ViewCards

--- src/components/view-cards.jsx
+++ src/components/view-cards.jsx
@@ -4,18 +4,114 @@ export default function ViewCards(props) {
   return (
     <React.Fragment>
       <h1>My Cards</h1>
-      <div className="row">
-        <div className="card text-white bg-dark mb-3 mx-2 col-2">
-          <div className="card-header card-img-top">
-            <h5 className="card-title">Question:</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content?</p>
+      <div className="mx-5 mt-5">
+        <div className="row row-cols-1 row-cols-md-3">
+
+          <div className="col mb-4">
+            <div className="card h-100">
+              <div className="card-header bg-dark text-white">
+                <h5 className="card-title text-light">Question:</h5>
+                <p className="card-text">Yo dude?</p>
+              </div>
+              <div className="card-body bg-secondary text-white">
+                <h5 className="card-title text-light">Answer:</h5>
+                <p className="card-text">suh dude</p>
+              </div>
+            </div>
           </div>
-          <div className="card-body">
-            <h5 className="card-title">Answer</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+          <div className="col mb-4">
+            <div className="card h-100">
+              <div className="card-header bg-dark text-white">
+                <h5 className="card-title text-light">Question:</h5>
+                <p className="card-text">Yo dude?</p>
+              </div>
+              <div className="card-body bg-secondary text-white">
+                <h5 className="card-title text-light">Answer:</h5>
+                <p className="card-text">suh dude</p>
+              </div>
+            </div>
           </div>
+
+          <div className="col mb-4">
+            <div className="card h-100">
+              <div className="card-header bg-dark text-white">
+                <h5 className="card-title text-light">Question:</h5>
+                <p className="card-text">Yo dude?</p>
+              </div>
+              <div className="card-body bg-secondary text-white">
+                <h5 className="card-title text-light">Answer:</h5>
+                <p className="card-text">suh dude</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="col mb-4">
+            <div className="card h-100">
+              <div className="card-header bg-dark text-white">
+                <h5 className="card-title text-light">Question:</h5>
+                <p className="card-text">Yo dude?</p>
+              </div>
+              <div className="card-body bg-secondary text-white">
+                <h5 className="card-title text-light">Answer:</h5>
+                <p className="card-text">suh dude</p>
+              </div>
+            </div>
+          </div>
+
         </div>
       </div>
+
+      {/* <div className="d-flex justify-content-center w-auto mx-auto">
+        <div className="row row-cols-4 my-5">
+
+          <div className="card text-white bg-dark mb-3 mx-2 col">
+            <div className="card-header card-img-top">
+              <h5 className="card-title">Question:</h5>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content?</p>
+            </div>
+            <div className="card-body">
+              <h5 className="card-title">Answer</h5>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+
+          <div className="card text-white bg-dark mb-3 mx-2 col">
+            <div className="card-header card-img-top">
+              <h5 className="card-title">Question:</h5>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content?</p>
+            </div>
+            <div className="card-body">
+              <h5 className="card-title">Answer</h5>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+
+          <div className="card text-white bg-dark mb-3 mx-2 col">
+            <div className="card-header card-img-top">
+              <h5 className="card-title">Question:</h5>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content?</p>
+            </div>
+            <div className="card-body">
+              <h5 className="card-title">Answer</h5>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+
+          <div className="card text-white bg-dark mb-3 mx-2 col">
+            <div className="card-header card-img-top">
+              <h5 className="card-title">Question:</h5>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content?</p>
+            </div>
+            <div className="card-body">
+              <h5 className="card-title">Answer</h5>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+
+        </div>
+      </div> */}
+
     </React.Fragment>
   );
 }
