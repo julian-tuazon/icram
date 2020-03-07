199e1694e5a4bcabb85e110088a861587c81f08a
Julian <jthrow00@gmail.com>
2020-03-07 15:06:39 -0800

    Added dynamic card creation of App state cards in ViewCards; changed this.passable from constructor property to const variable in App render

--- src/components/view-cards.jsx
+++ src/components/view-cards.jsx
@@ -2,28 +2,29 @@ import React from 'react';
 import {AppContext} from './app-context';
 
 export default function ViewCards(props) {
+  const context = React.useContext(AppContext);
+
+  const cards = context.cards.map(card =>
+    <div className="col mb-4">
+      <div className="card h-100">
+        <div className="card-header bg-dark text-white">
+          <h5 className="card-title text-light">Question:</h5>
+          <p className="card-text">{card.question}</p>
+        </div>
+        <div className="card-body bg-secondary text-white">
+          <h5 className="card-title">Answer:</h5>
+          <p className="card-text">{card.answer}</p>
+        </div>
+      </div>
+    </div>
+  );
+
   return (
-    // <AppContext.Consumer>
-    //   {context => context}
-    // </AppContext.Consumer>
     <React.Fragment>
       <h1>My Cards</h1>
       <div className="mx-5 mt-5">
         <div className="row row-cols-1 row-cols-md-3">
-
-          <div className="col mb-4">
-            <div className="card h-100">
-              <div className="card-header bg-dark text-white">
-                <h5 className="card-title text-light">Question:</h5>
-                <p className="card-text">Yo dude?</p>
-              </div>
-              <div className="card-body bg-secondary text-white">
-                <h5 className="card-title">Answer:</h5>
-                <p className="card-text">suh dude</p>
-              </div>
-            </div>
-          </div>
-
+          {cards}
         </div>
       </div>
     </React.Fragment>
