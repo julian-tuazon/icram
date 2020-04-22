dc4004ab697f51f09441c07ddab57dbd8f2c62b3
Julian <jthrow00@gmail.com>
2020-04-22 12:45:37 -0700

    Modified ReviewCards component to access AppContext; added componentDidMount to setActiveCard to first card in cards arr; created currentSide property in state

--- src/components/review-cards.jsx
+++ src/components/review-cards.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
 export default class ReviewCards extends React.Component {
+  constructor(props) {
+    this.state = { currentSide: '' };
+  }
+  componentDidMount() {
+    if (this.context.cards.length) this.context.setActiveCard(this.context.cards[0]);
+  }
   render() {
     return <h1>Review Cards</h1>;
   }
 }
+
+ReviewCards.contextType = AppContext;
