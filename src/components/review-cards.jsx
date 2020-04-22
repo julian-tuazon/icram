f201d01c349d148c60193ee4206f507c3f4101b0
Julian <jthrow00@gmail.com>
2020-04-22 12:54:19 -0700

    Added nextCard method to ReviewCards

--- src/components/review-cards.jsx
+++ src/components/review-cards.jsx
@@ -4,9 +4,18 @@ export default class ReviewCards extends React.Component {
   constructor(props) {
     this.state = { currentSide: '' };
   }
+
   componentDidMount() {
     if (this.context.cards.length) this.context.setActiveCard(this.context.cards[0]);
   }
+
+  nextCard() {
+    if (this.context.cards.length) {
+      const index = this.context.cards.findIndex(this.context.activeCard);
+      this.context.setActiveCard(++index % this.context.cards.length);
+    }
+  }
+
   render() {
     return <h1>Review Cards</h1>;
   }
