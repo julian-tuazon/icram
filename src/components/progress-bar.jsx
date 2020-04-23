0fc374ed729d27b39ef6f0347524f96e755b5296
Julian <jthrow00@gmail.com>
2020-04-23 16:24:08 -0700

    Modified barWidth of ProgressBar to be an object with css declarations as object key value pairs

--- src/components/progress-bar.jsx
+++ src/components/progress-bar.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
 export default function ProgressBar(props) {
-  const barWidth = `width: ${props.percentComplete}%`;
+  const barWidth = { width: `${props.percentComplete}%` };
   return (
-    <div class="progress">
-      <div class="progress-bar" role="progressbar" style={barWidth} aria-valuenow={props.percentComplete} aria-valuemin="0" aria-valuemax="100"></div>
+    <div className="progress progress-bar-height mb-3">
+      <div className="progress-bar" role="progressbar" style={barWidth} aria-valuenow={props.percentComplete} aria-valuemin="0" aria-valuemax="100"></div>
     </div>
   );
 }
