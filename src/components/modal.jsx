a4515e88b65fbf041bf69f60d416d4dd40b50f97
Julian <jthrow00@gmail.com>
2020-04-23 08:48:25 -0700

    Modified Modal to receive context; modified deleteCard to call context deleteCard; renamed showModal to modalClassName; styled modal delete text; set modal text from props

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { AppContext } from './app-context';
 
 export default class Modal extends React.Component {
   constructor(props) {
@@ -18,52 +19,52 @@ export default class Modal extends React.Component {
   }
 
   deleteCard() {
+    this.context.deleteCard(this.props.card);
     this.close();
-    console.log('goodbye card!');
   }
 
   render() {
-    const showModal = this.state.isOpen ? 'modal overlay d-block': 'modal overlay';
+    const modalClassName = this.state.isOpen ? 'modal overlay d-block' : 'modal overlay';
 
-    // if (this.state.isOpen) {
-      return (
-        <>
-          <div className="card-footer bg-dark">
-            <i className="far fa-trash-alt btn text-secondary" onClick={this.open}></i>
-          </div>
+    return (
+      <>
+        <div className="card-footer bg-dark">
+          <i className="far fa-trash-alt btn text-secondary" onClick={this.open}></i>
+        </div>
 
-          <div className={showModal} tabIndex="-1" role="dialog">
-            <div className="modal-dialog modal-dialog-centered" role="document">
-              <div className="modal-content">
-                <div className="modal-header">
-                  <h5 className="modal-title">Confirm Delete Card</h5>
-                  <button type="button" className="close" data-dismiss="modal" aria-label="Close" onClick={this.close}>
-                    <span aria-hidden="true">&times;</span>
-                  </button>
-                </div>
-                <div className="modal-body">
-                  <p>
-                    <i className="far fa-times-circle text-danger mr-2"></i>
-                    Delete card?
-                  </p>
-                </div>
-                <div className="modal-footer">
-                  <div className="btn-group w-75 mx-auto">
-                    <button type="button" id="remove" className="btn btn-outline-danger w-25" onClick={this.deleteCard}>Delete</button>
-                    <button type="button" id="cancel" className="btn btn-outline-secondary w-25" onClick={this.close} data-dismiss="modal">Cancel</button>
+        <div className={modalClassName} tabIndex="-1" role="dialog">
+          <div className="modal-dialog modal-dialog-centered" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title">Confirm Delete Card</h5>
+                <button type="button" className="close" data-dismiss="modal" aria-label="Close" onClick={this.close}>
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div className="modal-body">
+                <div className="mb-3">
+                  <i className="far fa-times-circle text-danger mr-2"></i>
+                    Are you sure you want to delete this card?
                   </div>
+                <div>
+                  <span className="font-weight-bold">Q:</span> {this.props.card.question}
+                </div>
+                <div>
+                  <span className="font-weight-bold">A:</span> {this.props.card.answer}
+                </div>
+              </div>
+              <div className="modal-footer">
+                <div className="btn-group w-75 mx-auto">
+                  <button type="button" id="remove" className="btn btn-outline-danger w-25" onClick={this.deleteCard}>Delete</button>
+                  <button type="button" id="cancel" className="btn btn-outline-secondary w-25" onClick={this.close} data-dismiss="modal">Cancel</button>
                 </div>
               </div>
             </div>
           </div>
-        </>
-      );
-    // }
-
-    // return (
-    //   <div className="card-footer bg-dark">
-    //     <i className="far fa-trash-alt btn text-secondary" onClick={this.open}></i>
-    //   </div>
-    // );
+        </div>
+      </>
+    );
   }
 }
+
+Modal.contextType = AppContext;
