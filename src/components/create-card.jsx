49afa494cc72deae210e236c3570326226c58ae9
Julian <jthrow00@gmail.com>
2020-04-24 12:43:05 -0700

    Removed ContextType from CreateCard

--- src/components/create-card.jsx
+++ src/components/create-card.jsx
@@ -12,5 +12,3 @@ export default function CreateCard(props) {
     </>
   );
 }
-
-CreateCard.contextType = AppContext;
--- src/components/update-card.jsx
+++ src/components/update-card.jsx
@@ -2,32 +2,16 @@ import React from 'react';
 import Form from './form';
 import { AppContext } from './app-context';
 
-export default class UpdateCard extends React.Component {
-  constructor(props) {
-    super(props);
-    this.updateCard = this.updateCard.bind(this);
-  }
+export default function UpdateCard(props) {
+  const context = React.useContext(AppContext);
+  const updateCard = card => this.context.updateCard(this.context.index, card);
+  const getQuestion = () => this.context.cards[this.context.index].question;
+  const getAnswer = () => this.context.cards[this.context.index].answer;
 
-  updateCard(card) {
-    this.context.updateCard(this.context.index, card);
-  }
-
-  getQuestion() {
-    return this.context.cards[this.context.index].question;
-  }
-
-  getAnswer() {
-    return this.context.cards[this.context.index].answer;
-  }
-
-  render() {
-    return (
-      <>
-        <h1 className="mb-5">Update Card</h1>
-        <Form cardAction={this.updateCard} question={this.getQuestion()} answer={this.getAnswer()} />
-      </>
-    );
-  }
+  return (
+    <>
+      <h1 className="mb-5">Update Card</h1>
+      <Form cardAction={this.updateCard} question={this.getQuestion()} answer={this.getAnswer()} />
+    </>
+  );
 }
-
-UpdateCard.contextType = AppContext;
