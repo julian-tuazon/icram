43371f8ee6242e5f95a5574fad32cfccfb2564cd
Julian <jthrow00@gmail.com>
2020-04-24 12:48:26 -0700

    Removed references to this in UpdateCard

--- src/components/update-card.jsx
+++ src/components/update-card.jsx
@@ -4,14 +4,14 @@ import { AppContext } from './app-context';
 
 export default function UpdateCard(props) {
   const context = React.useContext(AppContext);
-  const updateCard = card => this.context.updateCard(this.context.index, card);
-  const getQuestion = () => this.context.cards[this.context.index].question;
-  const getAnswer = () => this.context.cards[this.context.index].answer;
+  const updateCard = card => context.updateCard(context.index, card);
+  const getQuestion = () => context.cards[context.index].question;
+  const getAnswer = () => context.cards[context.index].answer;
 
   return (
     <>
       <h1 className="mb-5">Update Card</h1>
-      <Form cardAction={this.updateCard} question={this.getQuestion()} answer={this.getAnswer()} />
+      <Form cardAction={updateCard} question={getQuestion()} answer={getAnswer()} />
     </>
   );
 }
