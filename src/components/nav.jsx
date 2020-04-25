251d89e982c6a7714154ac3865f3ca4e62071466
Julian <jthrow00@gmail.com>
2020-04-24 17:40:17 -0700

    Modified Nav styling to highlight the current view with a solid color btn, background view is just text

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -6,16 +6,22 @@ export default class Nav extends React.Component {
     this.handleClick = this.handleClick.bind(this);
   }
 
+
+
   handleClick(e) {
     this.props.setView(e.target.id);
   }
 
   render() {
+    const viewCardsClassName = this.props.view === 'view-cards' ? 'mr-2 btn btn-primary' : 'mr-2 btn text-primary';
+    const reviewCardsClassName = this.props.view === 'review-cards' ? 'mr-2 btn btn-primary' : 'mr-2 btn text-primary';
+    const createCardClassName = this.props.view === 'create-card' ? 'btn btn-primary' : 'btn text-primary';
+
     return (
       <div className='d-flex justify-content-center my-4'>
-        <span className='mr-2 btn btn-primary' id='view-cards' onClick={this.handleClick}>View Cards</span>
-        <span className='mr-2 btn btn-info' id='review-cards' onClick={this.handleClick}>Review</span>
-        <span className='btn btn-success' id='create-card' onClick={this.handleClick}>Create Card</span>
+        <span className={viewCardsClassName} id='view-cards' onClick={this.handleClick}>View Cards</span>
+        <span className={reviewCardsClassName} id='review-cards' onClick={this.handleClick}>Review</span>
+        <span className={createCardClassName} id='create-card' onClick={this.handleClick}>Create Card</span>
       </div>
     );
   }
