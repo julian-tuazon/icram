e34c21157dd17ca7bbd1eda99700bc8ce7e15b2a
Julian <jthrow00@gmail.com>
2020-04-24 17:46:52 -0700

    Converted Nav into functional component

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,28 +1,16 @@
 import React from 'react';
 
-export default class Nav extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
-  }
+export default function Nav(props) {
+  const handleClick = e => props.setView(e.target.id);
+  const viewCardsClassName = props.view === 'view-cards' ? 'mr-2 btn btn-primary' : 'mr-2 btn text-primary';
+  const reviewCardsClassName = props.view === 'review-cards' ? 'mr-2 btn btn-primary' : 'mr-2 btn text-primary';
+  const createCardClassName = props.view === 'create-card' ? 'btn btn-primary' : 'btn text-primary';
 
-
-
-  handleClick(e) {
-    this.props.setView(e.target.id);
-  }
-
-  render() {
-    const viewCardsClassName = this.props.view === 'view-cards' ? 'mr-2 btn btn-primary' : 'mr-2 btn text-primary';
-    const reviewCardsClassName = this.props.view === 'review-cards' ? 'mr-2 btn btn-primary' : 'mr-2 btn text-primary';
-    const createCardClassName = this.props.view === 'create-card' ? 'btn btn-primary' : 'btn text-primary';
-
-    return (
-      <div className='d-flex justify-content-center my-4'>
-        <span className={viewCardsClassName} id='view-cards' onClick={this.handleClick}>View Cards</span>
-        <span className={reviewCardsClassName} id='review-cards' onClick={this.handleClick}>Review</span>
-        <span className={createCardClassName} id='create-card' onClick={this.handleClick}>Create Card</span>
-      </div>
-    );
-  }
+  return (
+    <div className='d-flex justify-content-center my-4'>
+      <span className={viewCardsClassName} id='view-cards' onClick={handleClick}>View Cards</span>
+      <span className={reviewCardsClassName} id='review-cards' onClick={handleClick}>Review</span>
+      <span className={createCardClassName} id='create-card' onClick={handleClick}>Create Card</span>
+    </div>
+  );
 }
