56aade20d16ec99738ff4db0d380201ddd88cc37
Julian <jthrow00@gmail.com>
2020-04-25 17:54:34 -0700

    Created Welcome component as greeting page for user

---
+++ src/components/welcome.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { AppContext } from './app-context';
+
+export default function Welcome(props) {
+  const context = React.useContext(AppContext);
+  const enterApp = () => context.setView('view-cards');
+
+  return (
+    <div className="container w-100 vh-100 d-flex justify-content-center align-items-center">
+      <div className="d-flex justify-content-center align-items-center flex-column">
+        <div className="d-flex align-items-center mb-3">
+          <h1 className="fade-slide">Flash Cards</h1>
+          <i className="fas fa-bolt fa-2x text-primary ml-3 fade-slide"></i>
+        </div>
+        <h5 className="mb-4 fade-slide">Study. Succeed.</h5>
+        <div>
+          <button className="btn btn-primary fade-slide-reverse" onClick={enterApp}>Let's Study</button>
+        </div>
+      </div>
+    </div>
+
+  );
+}
