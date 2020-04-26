f4544120be13518487ea36816801fbe61d6f0f5f
Julian <jthrow00@gmail.com>
2020-04-25 18:34:17 -0700

    Changed title to iCram

--- src/components/welcome.jsx
+++ src/components/welcome.jsx
@@ -9,10 +9,10 @@ export default function Welcome(props) {
     <div className="container w-100 vh-100 d-flex justify-content-center align-items-center">
       <div className="d-flex justify-content-center align-items-center flex-column">
         <div className="d-flex align-items-center mb-3">
-          <h1 className="fade-slide">Flash Cards</h1>
+          <h1 className="fade-slide">iCram</h1>
           <i className="fas fa-bolt fa-2x text-primary ml-3 fade-slide"></i>
         </div>
-        <h5 className="mb-4 fade-slide">Study. Succeed.</h5>
+        <h5 className="mb-5 fade-slide">Study. Succeed.</h5>
         <div>
           <button className="btn btn-primary fade-slide-reverse" onClick={enterApp}>Let's Study</button>
         </div>
